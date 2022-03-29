55f0755d82fff45016ca5dd56e2d473610b90a2d
Ermin Omeragić <erminnn@hotmail.com>
2022-03-29 10:48:26 +0200

    Added env && mongodb database

--- src/server.js
+++ src/server.js
@@ -1,5 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+// Load .env variables
+dotenv.config();
+// Connect to database
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(-1);
+  });
 
 const app = express();
 
@@ -8,5 +23,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Server running on port 3000"));
