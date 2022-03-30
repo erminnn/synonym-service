90bd565f471a3105418f3cc39ed63f58d455d1a1
Ermin Omeragić <erminnn@hotmail.com>
2022-03-30 13:04:48 +0200

    Changed type of synonyms in word model.Replaced ObjectId with array of ObjectIds

--- src/models/word.js
+++ src/models/word.js
@@ -2,10 +2,12 @@ import { model, Schema } from 'mongoose';
 
 const WordSchema = new Schema({
     name: String,
-    synonyms: {
-        type: Schema.Types.ObjectId,
-        ref: 'synonym'
-    }
+    synonyms: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'synonym'
+        }
+    ]
 });
 
 const WordModel = model('word', WordSchema);
