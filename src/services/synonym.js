99084d2b8a8150e5497a331004896c86a59bb6b2
Ermin Omeragić <erminnn@hotmail.com>
2022-03-30 13:06:26 +0200

    Added method for adding new synonyms to existing synonym record

--- src/services/synonym.js
+++ src/services/synonym.js
@@ -4,6 +4,11 @@ const addSynonyms = async (synonymPayload) => {
     return await new Synonym(synonymPayload).save();
 };
 
+const addNewSynonymsToExistingSynonym = async (synonymId, synonyms) => {
+    await Synonym.findByIdAndUpdate(synonymId, { $push: { synonyms: synonyms } }, { new: true });
+};
+
 export default {
-    addSynonyms
+    addSynonyms,
+    addNewSynonymsToExistingSynonym
 };
