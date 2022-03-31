3bf4cbe70041980a4c311857e81b218f844e29cf
Ermin Omeragić <erminnn@hotmail.com>
2022-03-31 11:45:35 +0200

    Added method for deleting multiple synonym record, also added method for getting synonyms sorted by synonyms size

--- src/services/synonym.js
+++ src/services/synonym.js
@@ -4,11 +4,36 @@ const addSynonyms = async (synonymPayload) => {
     return await new Synonym(synonymPayload).save();
 };
 
-const addNewSynonymsToExistingSynonym = async (synonymId, synonyms) => {
+const addNewWordsToExistingSynonym = async (synonyms, synonymId) => {
     await Synonym.findByIdAndUpdate(synonymId, { $push: { synonyms: synonyms } }, { new: true });
+};
+
+const findSynonymsFromArray = async (array) => {
+    return await Synonym.aggregate([
+        {
+            $match: {
+                _id: { $in: array }
+            }
+        },
+        {
+            $project: {
+                synonyms: 1,
+                length: { $size: '$synonyms' }
+            }
+        },
+
+        // Sort on the "synonyms size"
+        { $sort: { length: -1 } }
+    ]);
+};
+
+const deleteSynonyms = async (synonyms) => {
+    return await Synonym.deleteMany({ _id: { $in: synonyms } });
 };
 
 export default {
     addSynonyms,
-    addNewSynonymsToExistingSynonym
+    addNewWordsToExistingSynonym,
+    findSynonymsFromArray,
+    deleteSynonyms
 };
