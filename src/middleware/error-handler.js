8f15e6fc1fa9c3ab04effe7b62f46eb7ee55591a
Ermin Omeragić <erminnn@hotmail.com>
2022-04-01 13:57:14 +0200

    Implemented error handling middleware

---
+++ src/middleware/error-handler.js
@@ -0,0 +1,13 @@
+export default async (err, req, res, next) => {
+    // Specific validation error handler
+    if (err.statusCode) res.status(err.statusCode).send(err);
+    // Generic error handler
+    else
+        res.status(err.statusCode || 500).send({
+            error: {
+                type: err.type || 'internal_server_error',
+                message: err.message || 'Unexpected internal server error.',
+                details: err.details
+            }
+        });
+};
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import router from './routes';
+import errorMiddleware from './middleware/error-handler';
 
 // Load .env variables
 dotenv.config();
@@ -25,6 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(process.env.BASE_PATH, router);
+
+app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running on port 3000'));
